7223b7995141265c53b66c08e752330128afa779
Devin Walter <jwaltede@jci.com>
2019-07-10 15:42:05 -0400

    updated link in brewery card

--- src/components/BreweryCard/BreweryCard.tsx
+++ src/components/BreweryCard/BreweryCard.tsx
@@ -39,41 +39,21 @@ export default class BreweryCard extends Component<IBreweryCardProps, IBreweryCa
             <div className="card">
                 <img className="card-img-top" src={ genericImage } alt="Generic Brewery" />
                 <div className="card-body">
-                <h5 className="card-title">{ brewery.name }</h5>
+                    <h5 className="card-title">{ brewery.name }</h5>
+                    { brewery.website_url ? 
+                        <a className="btn btn-link card-text" 
+                            href={ brewery.website_url } 
+                            target="_blank"
+                            rel="noopener noreferrer">
+                                { brewery.website_url }
+                        </a> : <span>&nbsp;</span>
+                    }
                     <p className="card-text">Brewery Type: { brewery.brewery_type }</p>
                     <p className="card-text">{ address }</p>
                     <p className="card-text">{ brewery.phone }</p>                        
-                    <div className="row">
-                        <div className="col-sm">
-                            <a className="btn btn-warning btn-block" href={ brewery.website_url } target="_blank" rel="noopener noreferrer">View</a>
-                        </div>
-                        <div className="col-sm">
-                            <button className="btn btn-primary btn-block" onClick={ this.showBreweryInfo }>Info</button>
-                        </div>
-                    </div>
+                    <button className="btn btn-primary btn-block" onClick={ this.showBreweryInfo }>View More Info</button>                    
                 </div>
             </div>
-
-
-            // <div className="col-sm col-xs-12 d-flex align-items-stretch mb-3 mt-4">
-            //     <div className="card text-center brewery-card">
-            //         <img className="card-img-top" src={ genericImage } alt="Generic Brewery" />
-            //         <div className="card-body">
-            //             <h5 className="card-title">{ brewery.name }</h5>
-            //             <p className="card-text">Brewery Type: { brewery.brewery_type }</p>
-            //             <p className="card-text">{ address }</p>
-            //             <p className="card-text">{ brewery.phone }</p>                        
-            //             <div className="row">
-            //                 <div className="col-sm">
-            //                     <a className="btn btn-warning btn-block" href={ brewery.website_url } target="_blank" rel="noopener noreferrer">View</a>
-            //                 </div>
-            //                 <div className="col-sm">
-            //                     <button className="btn btn-primary btn-block" onClick={ this.showBreweryInfo }>Info</button>
-            //                 </div>
-            //             </div>
-            //         </div>
-            //     </div>
-            // </div>
         );
     }
 }

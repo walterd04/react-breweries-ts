c50ff52f8be8dbe08349b367fc701fdcac48885e
Devin Walter <walterd04@mounties.mansfield.edu>
2019-07-12 15:03:13 -0400

    it's working, the minimum requirements are met, and looks decent, but I think I still have some ui kinks to work out, probably a new design as well

--- src/components/BreweryInfo/BreweryInfo.tsx
+++ src/components/BreweryInfo/BreweryInfo.tsx
@@ -2,6 +2,7 @@ import React, { SFC } from 'react';
 import GoogleMapReact from 'google-map-react';
 // import MapMarker from '../MapMarker/MapMarker';
 import IBrewery from '../../interfaces/IBrewery';
+import './BreweryInfo.scss';
 
 //TODO: separate these out into their own files
 
@@ -28,13 +29,21 @@ const BreweryInfo: SFC<IBreweryInfoProps> = (props) => {
     }
 
     return (
-        <div style={{ height: '50em', width: '100%' }}>
-            <GoogleMapReact 
-                bootstrapURLKeys={{ key: 'AIzaSyBCn7UYYYzHCnv_qZ7sHY89gBbKjktRb58' }}
-                //google api key goes above
-                defaultCenter={ defaultCenter }
-                defaultZoom={ 11 }
-                onGoogleApiLoaded={ ({ map, maps }) => renderMarkers(map, maps) }></GoogleMapReact>
+        <div className="col-xs-10 col-xs-offset-1">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{ brewery.name }</h5>
+                    <p className="card-text">{ address }</p>
+                </div>
+                <div className="card-img-bottom" style={{ height: '50em', width: '100%' }}>
+                    <GoogleMapReact 
+                        bootstrapURLKeys={{ key: 'AIzaSyBCn7UYYYzHCnv_qZ7sHY89gBbKjktRb58' }}
+                        //google api key goes above
+                        defaultCenter={ defaultCenter }
+                        defaultZoom={ 11 }
+                        onGoogleApiLoaded={ ({ map, maps }) => renderMarkers(map, maps) }></GoogleMapReact>
+                </div>
+            </div>            
         </div>
     );
 }

14c0644b7e4fe6c177f5a98ae7b1dc991a72accf
Devin Walter <walterd04@mounties.mansfield.edu>
2019-07-13 12:54:30 -0400

    changed BreweryCard to stateless component

--- src/components/BreweryCard/BreweryCard.tsx
+++ src/components/BreweryCard/BreweryCard.tsx
@@ -11,48 +11,25 @@ interface IBreweryCardProps {
     active: boolean
 }
 
-interface IBreweryCardState {
-    brewery: IBrewery, 
-    showBreweryInfo: boolean
+const BreweryCard: React.SFC<IBreweryCardProps> = (props) => {
+    let { brewery, info } = props;
+    let address = `${ brewery.street } ${ brewery.city }, ${ brewery.state } ${ brewery.postal_code } ${ brewery.country }`;
+    let cardClass = props.active ? "list-group-item bg-dark text-white w-100" : "list-group-item bg-transparent w-100";
+
+    return (
+        <button className={ cardClass } onClick={ info }>
+            <div>
+                <h6>{ brewery.name }</h6>
+                <p>{ brewery.brewery_type }</p>
+                <p>{ address }</p>
+                <small className="text-muted">
+                    <a href={ brewery.website_url } target="_blank" rel="noopener noreferrer">
+                        { brewery.website_url }
+                    </a>
+                </small>
+            </div> 
+        </button>
+    );
 }
 
-export default class BreweryCard extends Component<IBreweryCardProps, IBreweryCardState> {
-    constructor(props: IBreweryCardProps) {
-        super(props);
-        this.state = {
-            brewery: this.props.brewery, 
-            showBreweryInfo: false
-        };
-    }
-
-    showBreweryInfo = () => { 
-        this.setState({ showBreweryInfo: true });
-    }
-
-    hideBreweryInfo = () => { 
-        this.setState({ showBreweryInfo: false });
-    }
-
-    render() { 
-        let { brewery, showBreweryInfo } = this.state;
-
-        let address = `${ brewery.street } ${ brewery.city }, ${ brewery.state } ${ brewery.postal_code } ${ brewery.country }`;
-
-        let cardClass = this.props.active ? "list-group-item bg-dark text-white w-100" : "list-group-item bg-transparent w-100";
-
-        return (
-            <button className={ cardClass } onClick={ this.props.info }>
-                <div>
-                    <h6>{ brewery.name }</h6>
-                    <p>{ brewery.brewery_type }</p>
-                    <p>{ address }</p>
-                    <small className="text-muted">
-                        <a href={ brewery.website_url } target="_blank" rel="noopener noreferrer">
-                            { brewery.website_url }
-                        </a>
-                    </small>
-                </div> 
-            </button>
-        );
-    }
-}
+export default BreweryCard;
